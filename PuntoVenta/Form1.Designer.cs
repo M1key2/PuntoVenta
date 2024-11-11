32eeb88b7e086707abc80f562a375cd440a8ed41
Miguel Angel Sanchez Peñuñuri <166767772+M1key2@users.noreply.github.com>
2024-11-11 11:47:13 -0700

    Add files via upload

---
+++ PuntoVenta/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace PuntoVenta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbProductos = new System.Windows.Forms.ListBox();
+            this.lbrecibo = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSub = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblIva = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbProductos
+            // 
+            this.lbProductos.FormattingEnabled = true;
+            this.lbProductos.Location = new System.Drawing.Point(54, 68);
+            this.lbProductos.Name = "lbProductos";
+            this.lbProductos.Size = new System.Drawing.Size(120, 95);
+            this.lbProductos.TabIndex = 0;
+            // 
+            // lbrecibo
+            // 
+            this.lbrecibo.FormattingEnabled = true;
+            this.lbrecibo.Location = new System.Drawing.Point(54, 201);
+            this.lbrecibo.Name = "lbrecibo";
+            this.lbrecibo.Size = new System.Drawing.Size(120, 95);
+            this.lbrecibo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Productos:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Recibo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Subtotal";
+            // 
+            // lblSub
+            // 
+            this.lblSub.AutoSize = true;
+            this.lblSub.Location = new System.Drawing.Point(316, 206);
+            this.lblSub.Name = "lblSub";
+            this.lblSub.Size = new System.Drawing.Size(34, 13);
+            this.lblSub.TabIndex = 5;
+            this.lblSub.Text = "$0.00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(254, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "IVA";
+            // 
+            // lblIva
+            // 
+            this.lblIva.AutoSize = true;
+            this.lblIva.Location = new System.Drawing.Point(319, 244);
+            this.lblIva.Name = "lblIva";
+            this.lblIva.Size = new System.Drawing.Size(34, 13);
+            this.lblIva.TabIndex = 7;
+            this.lblIva.Text = "$0.00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(254, 282);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Total";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblTotal.Location = new System.Drawing.Point(322, 281);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(34, 13);
+            this.lblTotal.TabIndex = 9;
+            this.lblTotal.Text = "$0.00";
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(242, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 44);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(242, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 43);
+            this.button2.TabIndex = 11;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(242, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 41);
+            this.button3.TabIndex = 12;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblIva);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblSub);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbrecibo);
+            this.Controls.Add(this.lbProductos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbProductos;
+        private System.Windows.Forms.ListBox lbrecibo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSub;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblIva;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ PuntoVenta/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoVenta
+{
+    public partial class Form1 : Form
+    {
+        String[,] productos = new string[5, 2] { { "coca", "20" }, { "sabritas", "15" }, { "atun", "15" }, { "mapa", "50" }, { "agua", "10" } };
+        String[] recibo;
+        double sub, iva, total;
+        public Form1()
+        {
+            InitializeComponent();
+            iniciarproductos();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void iniciarproductos()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                lbProductos.Items.Add(productos[i,0]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbrecibo.Items.Add(productos[lbProductos.SelectedIndex,0]);
+            sub += Convert.ToDouble(productos[lbProductos.SelectedIndex, 1]);
+            iva = sub * .16;
+            lblIva.Text = "$"+iva.ToString();
+            lblSub.Text = "$"+sub.ToString();
+            total = sub + iva;
+            lblTotal.Text = "$" + total.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (lbrecibo.SelectedIndex != -1)
+            {
+                switch (lbrecibo.SelectedItem)
+                {
+                    case "coca":
+                        sub = sub - 20;
+                        break;
+                    case "sabritas":
+                        sub = sub - 15;
+                        break;
+                    case "atun":
+                        sub = sub - 15;
+                        break;
+                    case "mota":
+                        sub = sub - 50;
+                        break;
+                    case "agua":
+                        sub = sub - 10;
+                        break;
+                }
+                lbrecibo.Items.RemoveAt(lbrecibo.SelectedIndex);
+
+                iva = sub * .16;
+                lblIva.Text = iva.ToString();
+                lblSub.Text = "$" + sub.ToString();
+                total = sub + iva;
+                lblTotal.Text = "$" + total.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please select an item to remove.");
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("desea confirmar la compra?","factura",MessageBoxButtons.YesNo)==DialogResult.No)
+            {
+
+            }
+            else
+            {
+               
+                lblSub.Text = "$0.00";
+                lblIva.Text = "$0.00";
+                lblTotal.Text = "$0.00";
+            }
+
+            
+            
+        }
+    }
+}
